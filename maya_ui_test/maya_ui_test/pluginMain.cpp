a6b68f9fe36206b5b440ae2da19eea468a42db65
martinnj <martinnjorgensen@gmail.com>
2013-06-02 21:00:09 +0200

    Added plugin registration of simple command
    Also added a solution file to the project.

--- maya_ui_test/maya_ui_test/pluginMain.cpp
+++ maya_ui_test/maya_ui_test/pluginMain.cpp
@@ -1,48 +1,61 @@
-//
-// Copyright (C) Martin Jørgensen 
 // 
 // File: pluginMain.cpp
 //
 // Author: Maya Plug-in Wizard 2.0
 //
-
+#include <stdio.h>
+#include <maya/MString.h>
+#include <maya/MArgList.h>
 #include <maya/MFnPlugin.h>
+#include <maya/MPxCommand.h>
+#include <maya/MIOStream.h>
+
+//#include <maya/MGlobal.h> // Needed to execute commands.
+
+class output : public MPxCommand
+{
+    public:
+        MStatus doIt( const MArgList& args );
+        static void* creator();
+};
+
+MStatus output::doIt( const MArgList& args ) {
+    cout << args.asString( 0 ).asChar() << endl;	
+    return MS::kSuccess;
+}
+
+void* output::creator() {
+    return new output();
+}
 
 MStatus initializePlugin( MObject obj )
-//
-//	Description:
-//		this method is called when the plug-in is loaded into Maya.  It 
-//		registers all of the services that this plug-in provides with 
-//		Maya.
-//
-//	Arguments:
-//		obj - a handle to the plug-in object (use MFnPlugin to access it)
-//
 { 
 	MStatus   status;
 	MFnPlugin plugin( obj, "Martin Jørgensen", "2013", "Any");
-
+	cout << "Registering plugin... ";
 	// Add plug-in feature registration here
 	//
+	plugin.registerCommand("write",output::creator);
 
+
+	cout << "Done" <<endl;
+	status = MS::kSuccess;
 	return status;
 }
 
 MStatus uninitializePlugin( MObject obj )
-//
-//	Description:
-//		this method is called when the plug-in is unloaded from Maya. It 
-//		deregisters all of the services that it was providing.
-//
-//	Arguments:
-//		obj - a handle to the plug-in object (use MFnPlugin to access it)
-//
 {
 	MStatus   status;
 	MFnPlugin plugin( obj );
 
+	cout << "Deregistering plugin... ";
 	// Add plug-in feature deregistration here
 	//
+	plugin.deregisterCommand("write");
 
+
+	
+	cout << "Done" <<endl;
+	status = MS::kSuccess;
 	return status;
 }
